7473a5303d16cb1c13cf9df9271943380cb896e3
akumar98394 <abhay98394@gmail.com>
2020-12-12 17:00:12 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const socket = require('socket.io');
+const app = express();
+
+app.use(express.static('public'));
+
+//Run the server
+const server = app.listen('5000',()=>{
+    console.log('Server running at 5000');
+});
+
+// setup sockets
+const io = socket(server);
+
+io.on('connection',(socket)=>{
+    console.log('Connected');
+    socket.on('update', (data) => {
+        console.log(data);
+        socket.broadcast.emit('update', data);
+    });
+    socket.on('play',()=>{
+        socket.broadcast.emit('play')
+    })
+    socket.on('pause',()=>{
+        socket.broadcast.emit('pause')
+    })
+    socket.on('slider',(data)=>{
+        socket.broadcast.emit('slider',data)
+    })
+});
