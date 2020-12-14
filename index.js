f601a82ceb657eba100443caa899e1eba20ec191
siriusblack009 <yjaiswal@cs.iitr.ac.in>
2020-12-14 12:24:39 +0530

    css added

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
-
-const express = require('express');
-const socket = require('socket.io');
+const express = require("express");
+const socket = require("socket.io");
 const app = express();
 var PORT = process.env.PORT || 5000;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 //Run the server
-const server = app.listen(PORT,()=>{
-    console.log('Server running at '+PORT);
+const server = app.listen(PORT, () => {
+  console.log("Server running at" + PORT);
 });
 
 // setup sockets
 const io = socket(server);
 
-io.on('connection',(socket)=>{
-    console.log('Connected');
-    socket.on('update', (data) => {
-        console.log(data);
-        socket.broadcast.emit('update', data);
-    });
-    socket.on('play',()=>{
-        socket.broadcast.emit('play')
-    })
-    socket.on('pause',()=>{
-        socket.broadcast.emit('pause')
-    })
-    socket.on('slider',(data)=>{
-        socket.broadcast.emit('slider',data)
-    })
+io.on("connection", (socket) => {
+  console.log("Connected");
+  socket.on("update", (data) => {
+    console.log(data);
+    socket.broadcast.emit("update", data);
+  });
+  socket.on("play", () => {
+    socket.broadcast.emit("play");
+  });
+  socket.on("pause", () => {
+    socket.broadcast.emit("pause");
+  });
+  socket.on("slider", (data) => {
+    socket.broadcast.emit("slider", data);
+  });
 });
