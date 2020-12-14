7bcdb6ea0fa6e5dafe44d46ceaa4e91a1cd90332
Saksham <sakshamdam54321@gmail.com>
2020-12-14 12:32:43 +0530

    fixed sync

--- public/index.js
+++ public/index.js
@@ -4,76 +4,77 @@ const slider = document.getElementById("custom-seekbar");
 
 let URL = window.URL || window.webkitURL;
 const displayMessage = function (message, isError) {
-  let element = document.getElementById("message");
-  element.innerHTML = message;
-  element.className = isError ? "error" : "info";
+	let element = document.getElementById("message");
+	element.innerHTML = message;
+	element.className = isError ? "error" : "info";
 };
 
 const playSelectedFile = function (event) {
-  let file = this.files[0];
-  let type = file.type;
-  let videoNode = document.querySelector("video");
-  let canPlay = videoNode.canPlayType(type);
-  if (canPlay === "") canPlay = "no";
-  let message = 'Can play type "' + type + '": ' + canPlay;
-  let isError = canPlay === "no";
-  // displayMessage(message, isError)
+	let file = this.files[0];
+	let type = file.type;
+	let videoNode = document.querySelector("video");
+	let canPlay = videoNode.canPlayType(type);
+	if (canPlay === "") canPlay = "no";
+	let message = 'Can play type "' + type + '": ' + canPlay;
+	let isError = canPlay === "no";
+	displayMessage(message, isError);
 
-  if (isError) {
-    return;
-  }
+	if (isError) {
+		return;
+	}
 
-  let fileURL = URL.createObjectURL(file);
-  videoNode.src = fileURL;
+	let fileURL = URL.createObjectURL(file);
+	videoNode.src = fileURL;
 };
 
 let inputNode = document.getElementById("input");
 inputNode.addEventListener("change", playSelectedFile, false);
 
 video.ontimeupdate = function () {
-  var percentage = (video.currentTime / video.duration) * 100;
-  $("#custom-seekbar span").css("width", percentage + "%");
-  socket.emit("update", percentage);
+	var percentage = (video.currentTime / video.duration) * 100;
+	$("#custom-seekbar span").css("width", percentage + "%");
+	socket.emit("update", percentage);
 };
 
 $("#custom-seekbar").on("click", function (e) {
-  var offset = $(this).offset();
-  var left = e.pageX - offset.left;
-  var totalWidth = $("#custom-seekbar").width();
-  var percentage = left / totalWidth;
-  var vidTime = video.duration * percentage;
-  video.currentTime = vidTime;
+	var offset = $(this).offset();
+	var left = e.pageX - offset.left;
+	var totalWidth = $("#custom-seekbar").width();
+	var percentage = left / totalWidth;
+	var vidTime = video.duration * percentage;
+	video.currentTime = vidTime;
+	playVideo();
 });
 
 // play event added
 function playVideo() {
-  socket.emit("play");
-  video.play();
-  let fraction = video.currentTime / video.duration;
-  video.currentTime = video.duration * fraction;
-  socket.emit("slider", video.currentTime);
+	socket.emit("play");
+	video.play();
+	let fraction = video.currentTime / video.duration;
+	video.currentTime = video.duration * fraction;
+	socket.emit("slider", video.currentTime);
 }
 
 // pause event handled
 function pauseVideo() {
-  socket.emit("pause");
-  video.pause();
+	socket.emit("pause");
+	video.pause();
 }
 
 // socket events handled
 socket.on("update", (data) => {
-  console.log("Recieved data", data);
-  $("#custom-seekbar span").css("width", data + "%");
+	console.log("Recieved data", data);
+	$("#custom-seekbar span").css("width", data + "%");
 });
 
 socket.on("play", () => {
-  video.play();
+	video.play();
 });
 
 socket.on("pause", () => {
-  video.pause();
+	video.pause();
 });
 
 socket.on("slider", (data) => {
-  video.currentTime = data;
+	video.currentTime = data;
 });
