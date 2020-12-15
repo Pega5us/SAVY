88446515056bb31983760738785d0ce032a08921
siriusblack009 <yjaiswal@cs.iitr.ac.in>
2020-12-15 12:16:26 +0530

    css for homepage added

--- public/index.js
+++ public/index.js
@@ -3,21 +3,21 @@
 let initialUrl = "https://sync-player666.herokuapp.com";
 
 function create() {
-	let response = httpGet(`${initialUrl}/getRoomNumber`);
-	let url = `${initialUrl}/room/${response}`;
-	window.location.href = url;
+  let response = httpGet(`${initialUrl}/getRoomNumber`);
+  let url = `${initialUrl}/room/${response}`;
+  window.location.href = url;
 }
 
 function join() {
-	const input = document.getElementById("input")
-	const response = input.value;
-	let url = `${initialUrl}/room/${response}`;
-	window.location.href = url;
+  const input = document.getElementById("input");
+  const response = input.value;
+  let url = `${initialUrl}/room/${response}`;
+  window.location.href = url;
 }
 
 function httpGet(theUrl) {
-	var xmlHttp = new XMLHttpRequest();
-	xmlHttp.open("GET", theUrl, false); // false for synchronous request
-	xmlHttp.send(null);
-	return xmlHttp.responseText;
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("GET", theUrl, false); // false for synchronous request
+  xmlHttp.send(null);
+  return xmlHttp.responseText;
 }
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static("public"));
 
 //Run the server
 const server = app.listen(PORT, () => {
-	console.log("Server running at " + PORT);
+  console.log("Server running at " + PORT);
 });
 
 // setup sockets
@@ -17,45 +17,49 @@ const io = socket(server);
 rooms = [];
 
 app.get("/room/:roomno", (req, res) => {
-	const roomno = req.params.roomno;
-	res.sendFile(join(__dirname, "public", "player.html"));
+  const roomno = req.params.roomno;
+  res.sendFile(join(__dirname, "public", "player.html"));
 });
 
 app.get("/getRoomNumber", (req, res) => {
-	console.log("Get Room");
-	let roomno;
-	do {
-		roomno = Math.floor(Math.random() * 10000 + 100000);
-	} while (rooms.includes(roomno));
-	res.send(`${roomno}`);
+  console.log("Get Room");
+  let roomno;
+  do {
+    roomno = Math.floor(Math.random() * 10000 + 100000);
+  } while (rooms.includes(roomno));
+  res.send(`${roomno}`);
 });
 
 app.get("/getPlayerCSS", (req, res) => {
-	res.sendFile(join(__dirname, "public", "player.css"));
+  res.sendFile(join(__dirname, "public", "player.css"));
+});
+
+app.get("/getPlayerDarkCSS", (req, res) => {
+  res.sendFile(join(__dirname, "public", "player1.css"));
 });
 
 app.get("/getPlayerJS", (req, res) => {
-	res.sendFile(join(__dirname, "public", "player.js"));
+  res.sendFile(join(__dirname, "public", "player.js"));
 });
 
 io.on("connection", (socket) => {
-	console.log("Connected");
+  console.log("Connected");
 
-	socket.on("joinroom", (roomno) => {
-		socket.join(roomno)
-		rooms.push(roomno);
-	})
-	socket.on("update", (data, roomno) => {
-		console.log(data);
-		socket.to(roomno).emit("update", data);
-	});
-	socket.on("play", (roomno) => {
-		socket.to(roomno).emit("play");
-	});
-	socket.on("pause", (roomno) => {
-		socket.to(roomno).emit("pause");
-	});
-	socket.on("slider", (data, roomno) => {
-		socket.to(roomno).emit("slider", data);
-	});
+  socket.on("joinroom", (roomno) => {
+    socket.join(roomno);
+    rooms.push(roomno);
+  });
+  socket.on("update", (data, roomno) => {
+    console.log(data);
+    socket.to(roomno).emit("update", data);
+  });
+  socket.on("play", (roomno) => {
+    socket.to(roomno).emit("play");
+  });
+  socket.on("pause", (roomno) => {
+    socket.to(roomno).emit("pause");
+  });
+  socket.on("slider", (data, roomno) => {
+    socket.to(roomno).emit("slider", data);
+  });
 });
