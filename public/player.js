a9702019d3c4919c26fec9a14cfd741e4b1bb9ae
Saksham <sakshamdam54321@gmail.com>
2020-12-20 12:14:00 +0530

    Refactored Player.js

--- public/player.js
+++ public/player.js
@@ -1,15 +1,19 @@
+// Getting roomno from URL
 let temp_arr = window.location.pathname.split("/");
 let roomno = temp_arr[temp_arr.length - 1];
-let arr = [];
+
+// Getting username from URL
 let current_username = new URLSearchParams(window.location.search).get(
 	"username"
 );
+
+// Buildig room URL for copy link button
 let room_URL = `https://sync-player666.herokuapp.com/room/${roomno}`;
 
-let spanEle = document.getElementById("roomNo");
-spanEle.innerText += roomno;
+document.getElementById("roomNo").innerText += roomno;
 document.getElementById("userDetail").innerText += current_username;
 
+// Initialising socket
 const socket = io();
 
 //Asking permission to enter the room
@@ -20,7 +24,7 @@ socket.on("room does not exist", () => {
 	window.location.href = "https://sync-player666.herokuapp.com";
 });
 
-$(".toast").toast("show");
+// $(".toast").toast("show");
 
 // Listenting for host reply
 socket.on("enter room", (isAllowed) => {
@@ -30,30 +34,33 @@ socket.on("enter room", (isAllowed) => {
 	else window.location.href = "https://sync-player666.herokuapp.com";
 });
 
-// For host to allow a user
+// Array to hold the pending permission of user to enter the room
 askingPermissionUsers = [];
 
+// Utility function to emit accept/deny permission
+// then delete the permission
+// and check for new permission
+function permissionSpliceAndCheckPermission(isAllowed) {
+	socket.emit("isAllowed", isAllowed, askingPermissionUsers[0].socketId);
+	askingPermissionUsers.splice(0, 1);
+	if (askingPermissionUsers.length !== 0) {
+		setTimeout(() => {
+			Utility();
+		}, 500);
+	}
+}
+
+// Decline new user from entering the room
 document.getElementById("decline-btn").onclick = () => {
-	socket.emit("isAllowed", false, askingPermissionUsers[0].socketId);
-	askingPermissionUsers.splice(0, 1);
-	if (askingPermissionUsers.length !== 0) {
-		console.log("declined");
-		setTimeout(() => {
-			Utility();
-		}, 500);
-	}
-};
-document.getElementById("accept-btn").onclick = () => {
-	socket.emit("isAllowed", true, askingPermissionUsers[0].socketId);
-	askingPermissionUsers.splice(0, 1);
-	if (askingPermissionUsers.length !== 0) {
-		setTimeout(() => {
-			Utility();
-		}, 500);
-		console.log("accepted");
-	}
+	permissionSpliceAndCheckPermission(false);
 };
 
+// Accept new user into the room
+document.getElementById("accept-btn").onclick = () => {
+	permissionSpliceAndCheckPermission(true);
+};
+
+// Utility function to show new permission to the host
 function Utility() {
 	document.getElementById(
 		"modal-body"
@@ -66,16 +73,13 @@ function Utility() {
 	$("#exampleModal").modal("show");
 }
 
+// New user permission
 socket.on("user permission", (username, socketId) => {
 	askingPermissionUsers.push({ username, socketId });
-	if (askingPermissionUsers.length !== 0)
-		setTimeout(() => {
-			Utility();
-		}, 500);
+	setTimeout(() => {
+		Utility();
+	}, 500);
 });
-
-// const video = document.getElementById("video");
-const slider = document.getElementById("custom-seekbar");
 
 function copyLink() {
 	console.log(room_URL);
@@ -90,14 +94,9 @@ function copyLink() {
 }
 
 let URL = window.URL || window.webkitURL;
-const displayMessage = function (message, isError) {
-	let element = document.getElementById("message");
-	element.innerHTML = message;
-	element.className = isError ? "error" : "info";
-};
-const video = new Plyr("#video");
 
-const playSelectedFile = function (event) {
+const video = new Plyr("#video");
+const playSelectedFile = function (_event) {
 	let file = this.files[0];
 	let fileURL = URL.createObjectURL(file);
 	video.source = {
@@ -113,11 +112,13 @@ const playSelectedFile = function (event) {
 	};
 };
 
-let inputNode = document.getElementById("input");
-inputNode.addEventListener("change", playSelectedFile, false);
+document
+	.getElementById("input")
+	.addEventListener("change", playSelectedFile, false);
 
 let canSeek = true;
-//play event handled
+
+//play event
 video.on("playing", (event) => {
 	console.log("video on playing ", canSeek);
 
@@ -127,14 +128,14 @@ video.on("playing", (event) => {
 	}
 });
 
-// pause event handled
+// pause event
 video.on("pause", (event) => {
 	console.log("video on pause ", canSeek);
 
 	if (canSeek) socket.emit("pause", roomno);
 });
 
-// seeking event handled
+// seeking event
 video.on("seeked", (event) => {
 	console.log("video on seeked " + canSeek);
 	if (canSeek) {
@@ -178,7 +179,33 @@ socket.on("seeked", (data) => {
 });
 
 const toastContainer = document.getElementById("toast-container");
+
 socket.on("new user", (username) => {
+	toastUserAddRemove(username, "joined");
+});
+
+socket.on("left room", (username) => {
+	toastUserAddRemove(username, "left");
+});
+
+socket.on("user_array", (user_array) => {
+	// Getting the array of users in room
+	document.getElementById("no_of_members").innerText = user_array.length;
+	let sidePanel = document.getElementById("sidePanel");
+	sidePanel.innerHTML = "";
+	let h = document.createElement("h3");
+	let txt = document.createTextNode("Connected Users:");
+	h.appendChild(txt);
+	sidePanel.appendChild(h);
+	user_array.map((users) => {
+		let para = document.createElement("p");
+		let node = document.createTextNode(users);
+		para.appendChild(node);
+		sidePanel.appendChild(para);
+	});
+});
+
+function toastUserAddRemove(username, eventHappened) {
 	toastContainer.innerHTML += `<div class="toast" data-autohide="false">
 					<div class="toast-header">
 						<svg
@@ -203,63 +230,11 @@ socket.on("new user", (username) => {
 						</button>
 					</div>
 					<div class="toast-body">
-						${username} has joined the room.
+						${username} has ${eventHappened} the room.
 					</div>
 				</div>`;
 	$(".toast").toast("show");
 	setTimeout(() => {
 		toastContainer.innerHTML = "";
 	}, 5000);
-});
-
-socket.on("left room", (username) => {
-	toastContainer.innerHTML += `<div class="toast" data-autohide="false">
-					<div class="toast-header">
-						<svg
-							class="rounded mr-2"
-							width="20"
-							height="20"
-							xmlns="http://www.w3.org/2000/svg"
-							preserveAspectRatio="xMidYMid slice"
-							focusable="false"
-							role="img"
-						>
-							<rect fill="#007aff" width="100%" height="100%" />
-						</svg>
-						<strong class="mr-auto">Notification</strong>
-						<button
-							type="button"
-							class="ml-2 mb-1 close"
-							data-dismiss="toast"
-							aria-label="Close"
-						>
-							<span aria-hidden="true">&times;</span>
-						</button>
-					</div>
-					<div class="toast-body">
-						${username} has left the room.
-					</div>
-				</div>`;
-	$(".toast").toast("show");
-	setTimeout(() => {
-		toastContainer.innerHTML = "";
-	}, 3000);
-});
-
-socket.on("user_array", (user_array) => {
-	// Getting the array of users in room
-	document.getElementById("no_of_members").innerText = user_array.length;
-	let z = document.getElementById("sidePanel");
-	z.innerHTML = "";
-	let h = document.createElement("h3");
-	let txt = document.createTextNode("Connected Users:");
-	h.appendChild(txt);
-	z.appendChild(h);
-	for (var i = 0; i < user_array.length; i++) {
-		let para = document.createElement("p");
-		let node = document.createTextNode(user_array[i]);
-		para.appendChild(node);
-		z.appendChild(para);
-	}
-	console.log(user_array);
-});
+}
