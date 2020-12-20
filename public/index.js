5595f971a583a9cc812c7bbe07f71ff8217ed99b
Saksham <sakshamdam54321@gmail.com>
2020-12-20 20:06:34 +0530

    Homepage UI changes

--- public/index.js
+++ public/index.js
@@ -3,7 +3,6 @@
 let initialUrl = "https://sync-player666.herokuapp.com";
 
 let errorMessageToast = document.getElementById("roomNotFoundErrorMessage");
-$(".toast").toast("hide");
 
 let current_roomno = new URLSearchParams(window.location.search).get("roomno");
 if (current_roomno) {
@@ -16,7 +15,7 @@ if (current_roomno) {
 }
 
 let username_local = localStorage.getItem("username");
-if (username_local !== "") {
+if (username_local !== null) {
 	document.getElementById("username").value = username_local;
 }
 
@@ -29,7 +28,7 @@ function create() {
 		window.location.href = url;
 	} else {
 		document.getElementById("username").style.boxShadow =
-			"5px 10px 18px #00897b";
+			"5px 5px 18px #00897b";
 		setTimeout(function () {
 			document.getElementById("username").style.boxShadow = "none";
 		}, 2000);
@@ -40,38 +39,37 @@ function join() {
 	const username = document.getElementById("username").value;
 	localStorage.setItem("username", username);
 	const roomno = document.getElementById("input").value;
-		if (username !== "" && roomno !== "") {
+	if (username !== "" && roomno !== "") {
 		let checkRoomExist = httpGet(`${initialUrl}/check/:roomno`);
 		if (!checkRoomExist) {
 			console.log("room found");
 			let url = `${initialUrl}/room/${roomno}?username=${username}`;
 			window.location.href = url;
 		} else {
-			console.log("showing toast");
-			$(".toast").toast("show");
+			document.getElementById("input").classList.add("is-invalid");
 		}
 		setTimeout(() => {
-			$(".toast").toast("hide");
-		}, 3000);
+			document.getElementById("input").classList.remove("is-invalid");
+		}, 1500);
 	} else {
 		if (username == "" && roomno == "") {
 			document.getElementById("username").style.boxShadow =
-				"5px 10px 18px #00897b";
+				"5px 5px 18px #00897b";
 			document.getElementById("input").style.boxShadow =
-				"5px 10px 18px #00897b";
+				"5px 5px 18px #00897b";
 			setTimeout(function () {
 				document.getElementById("username").style.boxShadow = "none";
 				document.getElementById("input").style.boxShadow = "none";
 			}, 2000);
 		} else if (username == "" && roomno !== "") {
 			document.getElementById("username").style.boxShadow =
-				"5px 10px 18px #00897b";
+				"5px 5px 18px #00897b";
 			setTimeout(function () {
 				document.getElementById("username").style.boxShadow = "none";
 			}, 2000);
 		} else {
 			document.getElementById("input").style.boxShadow =
-				"5px 10px 18px #00897b";
+				"5px 5px 18px #00897b";
 			setTimeout(function () {
 				document.getElementById("input").style.boxShadow = "none";
 			}, 2000);
@@ -85,3 +83,14 @@ function httpGet(theUrl) {
 	xmlHttp.send(null);
 	return xmlHttp.responseText;
 }
+
+document.getElementById("input").onkeypress = function (e) {
+	if (e.keyCode == 13) {
+		document.getElementById("key").onclick();
+	}
+};
+document.getElementById("username").onkeypress = function (e) {
+	if (e.keyCode == 13) {
+		document.getElementById("new_room").onclick();
+	}
+};
