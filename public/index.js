c61ccab989830ac4cc53bbef4ed2e6e908e6afab
siriusblack009 <yjaiswal@cs.iitr.ac.in>
2020-12-28 18:18:19 +0530

    minor UI changes on home page

--- public/index.js
+++ public/index.js
@@ -7,6 +7,8 @@ let userId = document.getElementById("username");
 let roomNo = document.getElementById("inputRoomNo");
 // get Room No
 let current_roomno = new URLSearchParams(window.location.search).get("roomno");
+let grp1 = document.getElementById("inputGroupPrepend3");
+let grp2 = document.getElementById("inputGroupPrepend4");
 
 if (current_roomno) {
 	let username = localStorage.getItem("username");
@@ -34,9 +36,13 @@ function create() {
 	}
 	// Else it will highlight the required field
 	else {
+		const x = userId.style.boxShadow;
+		const y = grp1.style.boxShadow;
 		userId.style.boxShadow = "5px 5px 18px #00897b";
+		grp1.style.boxShadow = "5px 5px 18px #00897b";
 		setTimeout(function () {
-			userId.style.boxShadow = "none";
+			userId.style.boxShadow = x;
+			grp1.style.boxShadow = y;
 		}, 2000);
 	}
 }
@@ -46,6 +52,8 @@ function join() {
 	const username = userId.value;
 	localStorage.setItem("username", username);
 	const roomno = roomNo.value;
+	const x = grp2.style.boxShadow;
+	const y = roomNo.style.boxShadow;
 	if (username !== "" && roomno !== "") {
 		let checkRoomExist = httpGet(`${initialUrl}/check/${roomno}`);
 		if (checkRoomExist === "true") {
@@ -54,31 +62,47 @@ function join() {
 			window.location.href = url;
 		} else {
 			roomNo.classList.add("is-invalid");
-			document.getElementById("inputGroupPrepend4").style.borderColor =
-				"red";
+			grp2.style.borderColor = "red";
+			grp2.style.color = "red";
+			roomNo.style.boxShadow = "none";
+			grp2.style.boxShadow = "none";
 		}
 		setTimeout(() => {
 			roomNo.classList.remove("is-invalid");
-			document.getElementById("inputGroupPrepend4").style.borderColor =
-				"white";
+			grp2.style.boxShadow = x;
+			roomNo.style.boxShadow = y;
+			grp2.style.borderColor = "white";
+			grp2.style.color = "#368755";
 		}, 1500);
 	} else {
+		const w = grp2.style.boxShadow;
+		const x = userId.style.boxShadow;
+		const y = roomNo.style.boxShadow;
+		const z = grp1.style.boxShadow;
 		if (username == "" && roomno == "") {
 			userId.style.boxShadow = "5px 5px 18px #00897b";
 			roomNo.style.boxShadow = "5px 5px 18px #00897b";
+			grp1.style.boxShadow = "5px 5px 18px #00897b";
+			grp2.style.boxShadow = "5px 5px 18px #00897b";
 			setTimeout(function () {
-				userId.style.boxShadow = "none";
-				roomNo.style.boxShadow = "none";
+				userId.style.boxShadow = x;
+				roomNo.style.boxShadow = y;
+				grp1.style.boxShadow = z;
+				grp2.style.boxShadow = w;
 			}, 2000);
 		} else if (username == "" && roomno !== "") {
 			userId.style.boxShadow = "5px 5px 18px #00897b";
+			grp1.style.boxShadow = "5px 5px 18px #00897b";
 			setTimeout(function () {
-				userId.style.boxShadow = "none";
+				userId.style.boxShadow = x;
+				grp1.style.boxShadow = z;
 			}, 2000);
 		} else {
 			roomNo.style.boxShadow = "5px 5px 18px #00897b";
+			grp2.style.boxShadow = "5px 5px 18px #00897b";
 			setTimeout(function () {
-				roomNo.style.boxShadow = "none";
+				roomNo.style.boxShadow = y;
+				grp2.style.boxShadow = w;
 			}, 2000);
 		}
 	}
@@ -108,22 +132,31 @@ userId.onkeypress = function (e) {
 
 //Modifying CSS of span field before input field
 function applyShadow1() {
-	var ele = document.getElementById("inputGroupPrepend4");
+	roomNo.style.boxShadow = "2px 0 0 2px #368755";
+	var ele = grp2;
 	ele.style.boxShadow = "-2px 0 0 2px #368755";
 	ele.style.backgroundColor = "#181a1b";
 }
 function removeShadow1() {
-	var ele = document.getElementById("inputGroupPrepend4");
+	roomNo.style.boxShadow = "none";
+	var ele = grp2;
 	ele.style.boxShadow = "none";
 	ele.style.backgroundColor = "none";
 }
 function removeShadow() {
-	var ele = document.getElementById("inputGroupPrepend3");
+	userId.style.boxShadow = "none";
+	var ele = grp1;
 	ele.style.boxShadow = "none";
 	ele.style.backgroundColor = "none";
 }
 function applyShadow() {
-	var ele = document.getElementById("inputGroupPrepend3");
+	userId.style.boxShadow = "2px 0 0 2px #368755";
+	var ele = grp1;
 	ele.style.boxShadow = "-2px 0 0 2px #368755";
 	ele.style.backgroundColor = "#181a1b";
+}
+
+function buttonFunc() {
+	document.getElementById("btn").style.border = "none";
+	document.getElementById("btn").style.boxShadow = "none";
 }
